294bf7915713177be51658b9321f5a9e7f36dd31
Kruglik Alexey <seldoff@gmail.com>
2018-08-30 01:30:25 +0300

    Initial app

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+using System.Windows;
+using System.Windows.Forms;
+using System.Windows.Interop;
+using Application = System.Windows.Application;
+
+namespace LidLock
+{
+    public partial class MainWindow : Window
+    {
+        [DllImport("user32.dll")]
+        private static extern void LockWorkStation();
+
+        [DllImport(@"User32", SetLastError = true, EntryPoint = "RegisterPowerSettingNotification",
+            CallingConvention = CallingConvention.StdCall)]
+        private static extern IntPtr RegisterPowerSettingNotification(IntPtr hRecipient, ref Guid PowerSettingGuid,
+            Int32 Flags);
+
+        internal struct POWERBROADCAST_SETTING
+        {
+            public Guid PowerSetting;
+            public uint DataLength;
+            public byte Data;
+        }
+
+        Guid GUID_LIDSWITCH_STATE_CHANGE = new Guid(0xBA3E0F4D, 0xB817, 0x4094, 0xA2, 0xD1, 0xD5, 0x63, 0x79, 0xE6, 0xA0, 0xF3);
+        const int DEVICE_NOTIFY_WINDOW_HANDLE = 0x00000000;
+        const int WM_POWERBROADCAST = 0x0218;
+        const int PBT_POWERSETTINGCHANGE = 0x8013;
+
+        private bool? _previousLidState = null;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            ShowInTaskbar = false;
+            ShowActivated = false;            
+            this.SourceInitialized += MainWindow_SourceInitialized;
+            Left = -10000;
+            Top = -10000;
+        }
+
+        void MainWindow_SourceInitialized(object sender, EventArgs e)
+        {
+            RegisterForPowerNotifications();
+            IntPtr hwnd = new WindowInteropHelper(this).Handle;
+            HwndSource.FromHwnd(hwnd).AddHook(new HwndSourceHook(WndProc));
+            Visibility = Visibility.Hidden;
+        }
+
+        private void RegisterForPowerNotifications()
+        {
+            IntPtr handle = new WindowInteropHelper(Application.Current.Windows[0]).Handle;
+            IntPtr hLIDSWITCHSTATECHANGE = RegisterPowerSettingNotification(handle,
+                 ref GUID_LIDSWITCH_STATE_CHANGE,
+                 DEVICE_NOTIFY_WINDOW_HANDLE);
+        }
+
+        IntPtr WndProc(IntPtr hwnd, int msg, IntPtr wParam, IntPtr lParam, ref bool handled)
+        {
+            switch (msg)
+            {
+                case WM_POWERBROADCAST:
+                    OnPowerBroadcast(wParam, lParam);
+                    break;
+                default:
+                    break;
+            }
+            return IntPtr.Zero;
+        }
+
+        private void OnPowerBroadcast(IntPtr wParam, IntPtr lParam)
+        {
+            if ((int)wParam == PBT_POWERSETTINGCHANGE)
+            {
+                POWERBROADCAST_SETTING ps = (POWERBROADCAST_SETTING)Marshal.PtrToStructure(lParam, typeof(POWERBROADCAST_SETTING));
+                IntPtr pData = (IntPtr)((int)lParam + Marshal.SizeOf(ps));
+                Int32 iData = (Int32)Marshal.PtrToStructure(pData, typeof(Int32));
+                if (ps.PowerSetting == GUID_LIDSWITCH_STATE_CHANGE)
+                {
+                    bool isLidOpen = ps.Data != 0;
+
+                    if (!isLidOpen == _previousLidState)
+                    {
+                        LidStatusChanged(isLidOpen);
+                    }
+
+                    _previousLidState = isLidOpen;
+                }
+            }
+        }
+
+        private void LidStatusChanged(bool isLidOpen)
+        {
+            if (!isLidOpen && Screen.AllScreens.Length == 1)
+            {
+                LockWorkStation();
+            }
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,9 @@
+# LidLock
+
+This app locks Windows session when laptop lid is closed UNLESS there is an external display connected to the laptop.
+
+This app does not have window or tray icon. If you want to close it you need to use Task Manager. 
+
+You probably want to start it at log on automatically. Use Task Scheduler for it. 
+
+You can build it yourself or use `LidLock.exe`. It requires .NET Framework 4.6 which is included with Windows 10.
